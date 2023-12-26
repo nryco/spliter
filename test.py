8ec9f97977b00c7a78a32c46a3907882a4fb2399
nrycox <cocoxytb@gmail.com>
2023-12-26 21:42:33 +0100

    e

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print('Hello Wrold')
+# 1. Source
+# 2. Duration  
+# 3. Parts
+
+class MyVideo:
+    def __init__(self, source, duration):
+
+            self.s = source
+
+            self.d = duration
