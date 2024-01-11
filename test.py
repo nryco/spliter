7b8623335b6e80680c76060da9f0d676a88541fe
Enrico <cocoxytb@gmail.com>
2024-01-11 20:17:49 +0100

    0

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
+import cv2
 # 1. Source
 # 2. Duration  
 # 3. Parts
+# 4. Format
 
 class MyVideo:
-    def __init__(self, source, duration):
+    def __init__(self, source, duration, format):
 
             self.s = source
 
             self.d = duration
+
+            self.p = parts
+
+            self.f = format
